fac8cd2e95c772d3756eb10dd53f36dd54e283f9
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-05-15 23:17:04 +0330

    redirect homepage to referral

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import {Route, Routes} from 'react-router-dom'
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Header from './components/common/Header'
+import Header from "./components/common/Header";
 
-import {RefreshContextProvider} from './context/RefreshContext'
-import {VeTHEsContextProvider} from './context/veTHEsConetext'
-import {BaseAssetsConetextProvider} from './context/BaseAssetsConetext'
-import {RouteAssetsConetextProvider} from './context/RouteAssetsConetext'
-import {PairsContextProvider} from './context/PairsContext'
+import { RefreshContextProvider } from "./context/RefreshContext";
+import { VeTHEsContextProvider } from "./context/veTHEsConetext";
+import { BaseAssetsConetextProvider } from "./context/BaseAssetsConetext";
+import { RouteAssetsConetextProvider } from "./context/RouteAssetsConetext";
+import { PairsContextProvider } from "./context/PairsContext";
 
-import Rewards from './pages/rewards'
-import Referral from './pages/referral'
+import Rewards from "./pages/rewards";
+import Referral from "./pages/referral";
 
 const App = () => {
   return (
-    <div className='main'>
+    <div className="main">
       <RefreshContextProvider>
         <BaseAssetsConetextProvider>
           <RouteAssetsConetextProvider>
             <PairsContextProvider>
               <VeTHEsContextProvider>
-              <Header/>
-              <Routes>
-                <Route path='/rewards' element={<Rewards/>} exact/>
-                <Route path='/referral' element={<Referral/>} exact/>
-              </Routes>
+                <Header />
+                <Routes>
+                  <Route path="/" element={<Navigate to="/referral" />} />
+                  <Route path="/rewards" element={<Rewards />} exact />
+                  <Route path="/referral" element={<Referral />} exact />
+                </Routes>
               </VeTHEsContextProvider>
             </PairsContextProvider>
           </RouteAssetsConetextProvider>
         </BaseAssetsConetextProvider>
       </RefreshContextProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
